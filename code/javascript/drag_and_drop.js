d7b71afbf8f2b904f45c99cce1bb7310c3ca8b72
lpilati <lpilati@fbk.eu>
2018-12-11 15:00:23 +0100

    Add the possibility to remove a product from a shopping list

--- code/javascript/drag_and_drop.js
+++ code/javascript/drag_and_drop.js
@@ -6,11 +6,26 @@ function drag(ev) {
 	ev.dataTransfer.setData("text", ev.target.id);
 }
 
+function deleteRow(event) {
+	var elem = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+	elem.remove();
+}
+
+function deleteUserRow(event) {
+	var elem = event.target.parentNode.parentNode;
+	elem.remove();
+}
+
+function addUserRow(event) {
+	//query
+}
+
 function drop(ev) {
 	ev.preventDefault();
 	var data = ev.dataTransfer.getData("text");
 	var div = document.getElementById(data);
 	var target = document.getElementById("shopping-list");
 	clone = div.cloneNode(true);
+	clone.children[0].rows[0].cells[2].innerHTML="<img src=\"../../images/basket.jpg\" style=\"padding-left:100px\" onclick=\"deleteRow(event)\"></img>";
 	target.appendChild(clone);
 }
